9cd57c8b0f01f7c22836fd7a71defd68a8a1e6ee
SoyokazeKuga <soyokazesutahamu@yahoo.co.jp>
2017-08-13 04:38:41 +0900

    エラーログ追加だけ

--- find-anaglam.py
+++ find-anaglam.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from bottle import route, redirect, post, run, template, request
+from bottle import route, redirect, post, run, template, request ,debug
 from anaglam.disassembly import disassembly
 from anaglam.grouping import get_group
 
+debug(True) #syykzが追加
 
 @route('/')
 def index():
@@ -16,6 +17,8 @@ def create():
     raw_str = request.forms.get('text')
     disassembly(raw_str)
     redirect('/')
+
+disassembly("あいうえお")
 
 if __name__ == '__main__':
     run(host='localhost', port=8080)
